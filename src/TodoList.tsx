7229c2bce2387dfe1be3afa25fbf359b5ac239b7
jy0813 <busbug3@gmail.com>
2023-02-21 11:30:51 +0900

    feat: todo setup

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function TodoList() {
+  const [todo, setTodo] = useState('');
+  const onChange = (e:React.FormEvent<HTMLInputElement>) => {
+    const {value} = e.currentTarget;
+    setTodo(value);
+  }
+  const onSubmit = (e:React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(todo);
+  }
   return (
-      <div></div>
+      <div>
+        <form onSubmit={onSubmit}>
+          <input onChange={onChange} value={todo} type="text" placeholder='Write a to do'/>
+          <button>Add</button>
+        </form>
+      </div>
   );
 }
 
